5734994421cf431192e58a98b7b5b32807e165c0
zyrndotio <3.00287385e+08+zyrndotio@users.noreply.github.com>
2026-09-10 15:07:36 +0000

    Fix Windows setup artifact selection

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::{
     io::Write,
     path::{Path, PathBuf},
 };
-const VERSION: &str = "0.4.5-beta.2";
+const VERSION: &str = "0.4.5-beta.3";
 
 fn install_path() -> Result<PathBuf, String> {
     let home = env::var_os("HOME")
